b4c395310b15326aecda511a2937d457621c7738
Kauane <kauane.nep@gmail.com>
2021-07-21 21:30:19 -0300

    Instancia do elemento de navegação entre telas

--- app/src/main/java/com/example/cartoeseveris/fragments/Login.kt
+++ app/src/main/java/com/example/cartoeseveris/fragments/Login.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.cartoeseveris.R
 
 class Login : Fragment() {
+
+    private lateinit var controllerInstance: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +19,9 @@ class Login : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        controllerInstance = Navigation.findNavController(view)
     }
 }
