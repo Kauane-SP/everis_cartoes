043a864511483e2c23025793accfebe17e4c8050
Kauane <kauane.nep@gmail.com>
2021-08-17 10:40:03 -0300

    Criação do layout do cartão

---
+++ app/src/main/java/com/example/cartoeseveris/api/cartoesApi/CartoesApiTask.kt
@@ -0,0 +1,4 @@
+package com.example.cartoeseveris.api.cartoesApi
+
+class CartoesApiTask {
+}
--- app/src/main/java/com/example/cartoeseveris/model/CartoesModel.kt
+++ app/src/main/java/com/example/cartoeseveris/model/CartoesModel.kt
@@ -3,8 +3,6 @@ package com.example.cartoeseveris.model
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
-
-@Parcelize
 data class CartoesModel(
     @SerializedName("name") val name: String
 )
