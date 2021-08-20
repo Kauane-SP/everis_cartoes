b56b269a6d7a97a2c6d52a42200f229e159990e9
Kauane <kauane.nep@gmail.com>
2021-08-20 10:33:48 -0300

    Criação de custon ship navigation

--- app/src/main/java/com/example/cartoeseveris/adapter/viewHolder/CardViewHolder.kt
+++ app/src/main/java/com/example/cartoeseveris/adapter/viewHolder/CardViewHolder.kt
@@ -5,13 +5,18 @@ import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cartoeseveris.R
 import com.example.cartoeseveris.model.CartoesModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CartoesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val numberCard: AppCompatTextView = itemView.findViewById(R.id.number_cartao)
+    private val data: AppCompatTextView = itemView.findViewById(R.id.data_card)
 
     fun onBind(cartoesModel: CartoesModel) {
 
+
         numberCard.text = cartoesModel.numberCartoes
+        data.text = cartoesModel.dataVencimento
     }
 }
